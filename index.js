f78840fa7aa71a79fbbbccac6086e61ab7443b55
Igor Barros <igorbarrosrj@gmail.com>
2020-02-11 18:25:43 -0300

    Modificação do Projeto com Middlewares.

--- index.js
+++ index.js
@@ -20,27 +20,38 @@ connection:{
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-
+//middleware
+app.use(async(req, res, next)=>{
+    const categories = await category.getCategories(db)()
+    res.locals = {
+        categories
+    }
+    next()
+})
 
 
 
 app.get('/', async(req, res)=> {
-    const categories = await category.getCategories(db)()
-       res.render('home', {
-           categories
-       })
-       
+       res.render('home')
+   
 })
 
 app.get('/categoria/:id/:slug', async(req, res)=>{
-    const categories = await category.getCategories(db)()
     const products = await product.getProductsByCategoryId(db)(req.params.id)
     const cat = await category.getCategoryById(db)(req.params.id)
     res.render('category',{
         products,
-        categories,
         category:cat
     })
+})
+
+app.get('/produto/:id/:slug', async(req,res) =>{
+    const prod = await product.getProductById(db)(req.params.id)
+    res.render('product-detail',{
+        product: prod        
+       
+    })
+
 })
 
 app.listen(port, err => {
