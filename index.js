702bff114d9b7424cc34b633cb54b2661acc810f
Igor Barros <igorbarrosrj@gmail.com>
2020-02-12 22:30:48 -0300

    Criando rotas e organizando o projeto.

--- index.js
+++ index.js
@@ -1,44 +1,20 @@
-const express = require ('express')
-const app = express()
+const db = require('knex')({
+    client: 'mysql2',
+    connection:{
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database:'devshop'
+    }
+})
+    db.on('query', query =>{
+        console.log('SQL: ', query.sql)
+    
+    })
+
+const app = require('./app')(db)
 const port = process.env.PORT || 3000
 
-
-const category = require('./models/category')
-const product = require('./models/product')
-
-
-const categories = require ('./controllers/categories')
-const products = require('./controllers/products')
-const home = require ('./controllers/home')
-
-const db = require('knex')({
-client: 'mysql2',
-connection:{
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database:'devshop'
-}
-
-})
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-
-//middleware
-app.use(async(req, res, next)=>{
-    const categories = await category.getCategories(db)()
-    res.locals = {
-        categories
-    }
-    next()
-})
-
-
-
-app.get('/', home.getIndex)
-app.get('/categoria/:id/:slug', categories.getCategories(db))
-app.get('/produto/:id/:slug', products.getProduct(db))
 
 app.listen(port, err => {
     if(err){
